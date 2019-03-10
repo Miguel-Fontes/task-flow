afdcad1a5565f587cf6e0bdf77d49408d8513fa4
Miguel Fontes <miguel_m_fontes@hotmail.com>
2019-03-10 09:59:17 -0300

    Task Repository interface

---
+++ task-flow-ports/src/main/java/br/com/miguelfontes/taskflow/ports/repository/TaskRepository.java
@@ -0,0 +1,28 @@
+package br.com.miguelfontes.taskflow.ports.repository;
+
+import br.com.miguelfontes.taskflow.core.tasks.Task;
+
+import java.util.List;
+
+/**
+ * Provides data access operations for {@link Task} entities
+ *
+ * @author Miguel Fontes
+ */
+public interface TaskRepository {
+
+   /**
+         * Persist a {@link Task} entity
+         *
+         * @param task the Task to be persisted
+         * @return a instance of Task with updated identifiers, if applicable
+         */
+    Task save(Task task);
+
+    /**
+          * Retrieves all  persisted {@link Task} entities
+          *
+          * @return a list containing all Task entities
+          */
+    List<Task> findAll();
+}
