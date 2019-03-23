01406d358c1efeae63685f6b4b22bc57cf4c6c34
Miguel <miguel_m_fontes@hotmail.com>
2019-03-23 17:59:24 -0300

    Added more tests

--- task-flow-tasks-test/src/test/java/br/com/miguelfontes/taskflow/tasks/UpdateTaskTest.java
+++ task-flow-tasks-test/src/test/java/br/com/miguelfontes/taskflow/tasks/UpdateTaskTest.java
@@ -8,7 +8,10 @@ import br.com.miguelfontes.taskflow.ports.tasks.TaskDTO;
 import br.com.miguelfontes.taskflow.ports.tasks.TasksAPI;
 import br.com.miguelfontes.taskflow.ports.tasks.UpdateTaskRequest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,7 +30,6 @@ class UpdateTaskTest {
     void shouldUpdateATaskTitle() {
         final String newTitle = "a new title";
         Task task = repository.save(TASK);
-
         var request = buildUpdateTaskTitleRequest(task, newTitle);
 
         TaskDTO response = service.execute(request).getTask();
@@ -50,7 +52,6 @@ class UpdateTaskTest {
     void shouldUpdateATaskDescription() {
         final String newDescription = "a new description";
         Task task = repository.save(TASK);
-
         var request = buildUpdateTaskDescriptionRequest(task, newDescription);
 
         TaskDTO response = service.execute(request).getTask();
@@ -68,4 +69,45 @@ class UpdateTaskTest {
     private UpdateTaskRequest buildUpdateTaskDescriptionRequest(Task task, String newDescription) {
         return UpdateTaskRequest.of(task.getId(), task.getTitle(), newDescription, task.getStatus().toString(), task.getAuthor().getId());
     }
+
+    @Nested
+    @DisplayName("task creation on update")
+    class TaskCreationOnUpdate {
+
+        @Test
+        @DisplayName("should create a new task if none is found with a given id")
+        void shouldCreateANewTaskIfNoneIsFoundWithGivenId() {
+            UUID newId = UUID.randomUUID();
+            Task task = repository.save(TASK);
+            var request = buildUpdateTaskRequestWithId(task, newId);
+
+            TaskDTO newTask = service.execute(request).getTask();
+
+            assertAll(
+                    () -> assertEquals(task.getTitle(), newTask.getTitle()),
+                    () -> assertEquals(task.getStatus().toString(), newTask.getStatus()),
+                    () -> assertEquals(task.getDescription(), newTask.getDescription()),
+                    () -> assertNotEquals(task.getId(), newTask.getId()),
+                    () -> assertNotEquals(task.getCreatedAt(), newTask.getCreatedAt())
+            );
+        }
+
+        @Test
+        @DisplayName("should generate a new id for the task")
+        void shouldGenerateANewIdForTheTask() {
+            UUID newId = UUID.randomUUID();
+            Task task = repository.save(TASK);
+            var request = buildUpdateTaskRequestWithId(task, newId);
+
+            TaskDTO newTask = service.execute(request).getTask();
+
+            assertNotEquals(newId, newTask.getId());
+        }
+
+        private UpdateTaskRequest buildUpdateTaskRequestWithId(Task task, UUID id) {
+            return UpdateTaskRequest.of(id, task.getTitle(), task.getDescription(), task.getStatus().toString(), task.getAuthor().getId());
+        }
+    }
+
+
 }
