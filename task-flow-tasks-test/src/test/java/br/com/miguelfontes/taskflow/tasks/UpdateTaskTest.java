e121c2e02b98e63542b5b8e2be2cbf4428e65c65
Miguel Fontes <miguel_m_fontes@hotmail.com>
2019-03-28 23:06:29 -0300

    Removed status and author fields, and creation behavior from update operation. (#13)

--- task-flow-tasks-test/src/test/java/br/com/miguelfontes/taskflow/tasks/UpdateTaskTest.java
+++ task-flow-tasks-test/src/test/java/br/com/miguelfontes/taskflow/tasks/UpdateTaskTest.java
@@ -5,6 +5,7 @@ import br.com.miguelfontes.taskflow.core.tasks.User;
 import br.com.miguelfontes.taskflow.persistence.mmdb.TaskRepositoryMMDB;
 import br.com.miguelfontes.taskflow.ports.persistence.TaskRepository;
 import br.com.miguelfontes.taskflow.ports.tasks.TaskDTO;
+import br.com.miguelfontes.taskflow.ports.tasks.TaskNotFoundException;
 import br.com.miguelfontes.taskflow.ports.tasks.TasksAPI;
 import br.com.miguelfontes.taskflow.ports.tasks.UpdateTaskRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -26,88 +27,64 @@ class UpdateTaskTest {
     private final TasksAPI service = TasksService.instance(repository);
 
     @Test
-    @DisplayName("should update a Task title")
-    void shouldUpdateATaskTitle() {
-        final String newTitle = "a new title";
+    @DisplayName("should throw exception when task is not found")
+    void shouldThrowExceptionWhenTaskIsNotFound() {
+        UUID nonExistentTaskId = UUID.randomUUID();
         Task task = repository.save(TASK);
-        var request = buildUpdateTaskTitleRequest(task, newTitle);
+        var request = buildUpdateTaskRequestWithId(task, nonExistentTaskId);
 
-        TaskDTO response = service.execute(request).getTask();
+        final var taskNotFoundException = assertThrows(TaskNotFoundException.class, () -> service.execute(request));
 
-        assertAll(
-                () -> assertEquals(task.getId(), response.getId()),
-                () -> assertEquals(task.getStatus().toString(), response.getStatus()),
-                () -> assertEquals(task.getCreatedAt(), response.getCreatedAt()),
-                () -> assertNotEquals(task.getTitle(), response.getTitle()),
-                () -> assertEquals(newTitle, response.getTitle())
-        );
+        assertTrue(taskNotFoundException.getMessage().contains(nonExistentTaskId.toString()));
     }
 
-    private UpdateTaskRequest buildUpdateTaskTitleRequest(Task task, String title) {
-        return UpdateTaskRequest.of(task.getId(), title, task.getDescription(), task.getStatus().toString(), task.getAuthor().getId());
-    }
-
-    @Test
-    @DisplayName("should update a task description")
-    void shouldUpdateATaskDescription() {
-        final String newDescription = "a new description";
-        Task task = repository.save(TASK);
-        var request = buildUpdateTaskDescriptionRequest(task, newDescription);
-
-        TaskDTO response = service.execute(request).getTask();
-
-        assertAll(
-                () -> assertEquals(task.getId(), response.getId()),
-                () -> assertEquals(task.getTitle(), response.getTitle()),
-                () -> assertEquals(task.getStatus().toString(), response.getStatus()),
-                () -> assertEquals(task.getCreatedAt(), response.getCreatedAt()),
-                () -> assertNotEquals(task.getDescription(), response.getDescription()),
-                () -> assertEquals(newDescription, response.getDescription())
-        );
-    }
-
-    private UpdateTaskRequest buildUpdateTaskDescriptionRequest(Task task, String newDescription) {
-        return UpdateTaskRequest.of(task.getId(), task.getTitle(), newDescription, task.getStatus().toString(), task.getAuthor().getId());
+    private UpdateTaskRequest buildUpdateTaskRequestWithId(Task task, UUID id) {
+        return UpdateTaskRequest.of(id, task.getTitle(), task.getDescription());
     }
 
     @Nested
-    @DisplayName("task creation on update")
-    class TaskCreationOnUpdate {
+    @DisplayName("when successful")
+    class WhenSuccessful {
+        final String newTitle = "a new title";
+        final String newDescription = "a new description";
+        final Task task = repository.save(TASK);
+        final TaskDTO updatedTask = service.execute(UpdateTaskRequest.of(task.getId(), newTitle, newDescription)).getTask();
 
         @Test
-        @DisplayName("should create a new task if none is found with a given id")
-        void shouldCreateANewTaskIfNoneIsFoundWithGivenId() {
-            UUID newId = UUID.randomUUID();
-            Task task = repository.save(TASK);
-            var request = buildUpdateTaskRequestWithId(task, newId);
-
-            TaskDTO newTask = service.execute(request).getTask();
-
-            assertAll(
-                    () -> assertEquals(task.getTitle(), newTask.getTitle()),
-                    () -> assertEquals(task.getStatus().toString(), newTask.getStatus()),
-                    () -> assertEquals(task.getDescription(), newTask.getDescription()),
-                    () -> assertNotEquals(task.getId(), newTask.getId()),
-                    () -> assertNotEquals(task.getCreatedAt(), newTask.getCreatedAt())
-            );
+        @DisplayName("should update a Task title")
+        void shouldUpdateATaskTitle() {
+            assertEquals(newTitle, updatedTask.getTitle());
         }
 
         @Test
-        @DisplayName("should generate a new id for the task")
-        void shouldGenerateANewIdForTheTask() {
-            UUID newId = UUID.randomUUID();
-            Task task = repository.save(TASK);
-            var request = buildUpdateTaskRequestWithId(task, newId);
-
-            TaskDTO newTask = service.execute(request).getTask();
-
-            assertNotEquals(newId, newTask.getId());
+        @DisplayName("should update a task description")
+        void shouldUpdateATaskDescription() {
+            assertEquals(newDescription, updatedTask.getDescription());
         }
 
-        private UpdateTaskRequest buildUpdateTaskRequestWithId(Task task, UUID id) {
-            return UpdateTaskRequest.of(id, task.getTitle(), task.getDescription(), task.getStatus().toString(), task.getAuthor().getId());
+        @Test
+        @DisplayName("should update task updatedAt field")
+        void shouldUpdateTaskUpdatedAtAttribute() {
+            assertNotEquals(task.getUpdatedAt(), updatedTask.getUpdatedAt());
+        }
+
+        @Test
+        @DisplayName("should not change task status")
+        void shouldNotChangeTaskStatus() {
+            assertEquals(task.getStatus().toString(), updatedTask.getStatus());
+        }
+
+        @Test
+        @DisplayName("should not change task author")
+        void shouldNotChangeTaskAuthor() {
+            assertEquals(task.getAuthor().getId(), updatedTask.getAuthor());
+        }
+
+        @Test
+        @DisplayName("should not change task id")
+        void shouldNotChangeTaskId() {
+            assertEquals(task.getId(), updatedTask.getId());
         }
     }
-
 
 }
