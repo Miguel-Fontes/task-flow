4af271718a07d550e11db4eebed0260f1e20551d
Miguel <miguel_m_fontes@hotmail.com>
2019-04-09 21:41:55 -0300

    Abrir APIs granulares para demais clientes

--- task-flow-ports-tasks/src/main/java/br/com/miguelfontes/taskflow/ports/tasks/CreateUser.java
+++ task-flow-ports-tasks/src/main/java/br/com/miguelfontes/taskflow/ports/tasks/CreateUser.java
@@ -6,6 +6,6 @@ package br.com.miguelfontes.taskflow.ports.tasks;
  *
  * @author Miguel Fontes
  */
-interface CreateUser {
+public interface CreateUser {
     CreateUserResponse execute(CreateUserRequest request);
 }
--- task-flow-ports-tasks/src/main/java/br/com/miguelfontes/taskflow/ports/tasks/UpdateTask.java
+++ task-flow-ports-tasks/src/main/java/br/com/miguelfontes/taskflow/ports/tasks/UpdateTask.java
@@ -5,6 +5,6 @@ package br.com.miguelfontes.taskflow.ports.tasks;
  *
  * @author Miguel Fontes
  */
-interface UpdateTask {
+public interface UpdateTask {
     UpdateTaskResponse execute(UpdateTaskRequest request);
 }
